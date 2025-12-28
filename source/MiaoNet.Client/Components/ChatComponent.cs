2f0db5ec5766854d9a3aa69fdb2abbd3bde9d5aa
Saplonily <Saplonily@outlook.com>
2025-12-28 17:24:10 +0800

    Support chat history

--- source/MiaoNet.Client/Components/ChatComponent.cs
+++ source/MiaoNet.Client/Components/ChatComponent.cs
@@ -11,9 +11,14 @@ public sealed class ChatComponent : MiaoNetComponent
     private readonly ChatMessageListView chatView;
     private readonly CommandParser cmdParser;
 
+    private string lastInput = string.Empty;
+    private readonly List<string> history;
+    private int historyIndex;
+
     public ChatComponent(MiaoNetContext context)
         : base(context)
     {
+        history = new();
         ITextRenderer r = new MiaoNetTextRenderer();
         inputBox = new InputBox(r);
         chatView = new(r);
@@ -63,6 +68,7 @@ public sealed class ChatComponent : MiaoNetComponent
                 string trimmedText = text.Trim();
                 if (trimmedText != string.Empty)
                 {
+                    history.Add(trimmedText);
                     if (!trimmedText.StartsWith(CommandParser.CommandPrefix))
                         SendChat(trimmedText);
                     else
@@ -71,11 +77,39 @@ public sealed class ChatComponent : MiaoNetComponent
                 Deactive();
                 return;
             }
+            if (MInput.Keyboard.Pressed(Keys.Up))
+            {
+                int i = historyIndex;
+                i -= 1;
+                if (i < 0) i = 0;
+                if (i != historyIndex)
+                {
+                    if (historyIndex == history.Count)
+                        lastInput = inputBox.Text;
+                    historyIndex = i;
+                    inputBox.SetText(history[i]);
+                }
+            }
+            else if (MInput.Keyboard.Pressed(Keys.Down))
+            {
+                int i = historyIndex;
+                i += 1;
+                if (i > history.Count)
+                    i = history.Count;
+                if (i != historyIndex)
+                {
+                    historyIndex = i;
+                    if (i == history.Count)
+                        inputBox.SetText(lastInput);
+                    else
+                        inputBox.SetText(history[i]);
+                }
+            }
             inputBox.Update();
         }
         chatView.Update();
     }
-    
+
     public void SendChat(string text)
         => context.QueuePacket(new PacketSendChatMessage(text));
 
@@ -88,7 +122,7 @@ public sealed class ChatComponent : MiaoNetComponent
     public void OnNotifyMessage(string text)
         => chatView.AddChatMessage(MiaoNetChatText.CreateAnnouncement(text));
 
-    public void OnSentPrivateMessage(OnlinePlayer other, string text) 
+    public void OnSentPrivateMessage(OnlinePlayer other, string text)
         => chatView.AddChatMessage(MiaoNetChatText.CreateSentPrivateChat(other, context.ClientState!.Self, text));
 
     public void HandleCommand(string text)
@@ -135,11 +169,14 @@ public sealed class ChatComponent : MiaoNetComponent
         if (active)
             Deactive();
         chatView.CleanUp();
+        history.Clear();
+        historyIndex = 0;
     }
 
     private void Active()
     {
         active = true;
+        historyIndex = history.Count;
         inputBox.Active();
         chatView.AlwaysShow = true;
         Engine.Scene.Paused = true;
@@ -149,6 +186,7 @@ public sealed class ChatComponent : MiaoNetComponent
     {
         active = false;
         inputBox.Deactive();
+        lastInput = string.Empty;
         chatView.AlwaysShow = false;
         Engine.Scene.Paused = false;
     }
