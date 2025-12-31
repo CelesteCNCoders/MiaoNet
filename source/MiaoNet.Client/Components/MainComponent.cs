8072648195422eb67e9ef094f5dde6a517c49616
Saplonily <Saplonily@outlook.com>
2025-12-31 13:27:06 +0800

    Self name tag fix

--- source/MiaoNet.Client/Components/MainComponent.cs
+++ source/MiaoNet.Client/Components/MainComponent.cs
@@ -43,6 +43,7 @@ public sealed class MainComponent : MiaoNetComponent
         foreach (var pair in ghosts)
             pair.Value.RemoveSelf();
         ghosts.Clear();
+        selfNameTag = null;
     }
 
     private void Player_OnDie(Player player)
