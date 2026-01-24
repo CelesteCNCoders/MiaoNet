7ce92373768c6a2e160dd731c486d2b6282524aa
Saplonily <Saplonily@outlook.com>
2026-01-24 14:49:25 +0800

    Set compression level to no compression

--- source/MiaoNet.Client/publishToZip.ps1
+++ source/MiaoNet.Client/publishToZip.ps1
@@ -2,5 +2,5 @@ $mod_name = "MiaoNet"
 dotnet build -c Release
 $v_str = Get-Content -Path ModFolder/everest.yaml -Raw
 $v = [regex]::Match($v_str, "(?<=Version:\s)(.*?)\n").Value.Trim()
-Compress-Archive ModFolder/* "$mod_name v$v.zip" -Force
+Compress-Archive ModFolder/* "$mod_name v$v.zip" -Force -CompressionLevel NoCompression
 dotnet clean
