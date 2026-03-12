461f83befd6b8cc4065a5e7ab89f90222f1c0e94
Saplonily <Saplonily@outlook.com>
2026-03-12 11:25:38 +0800

    Misc watch fixes and improvements

--- source/MiaoNet.Client/Components/MainComponent.Watching.cs
+++ source/MiaoNet.Client/Components/MainComponent.Watching.cs
@@ -16,12 +16,36 @@ public sealed partial class MainComponent
                 StopWatching();
                 return;
             }
+            var selfLoc = ClientState.Self.Location;
+            var otherLoc = playerWatching.Location;
+            if (selfLoc.MapRoom != otherLoc.MapRoom && !otherLoc.IsInDebugMap && level.transition is null)
+            {
+                var otherPos = playerWatching.State.Position;
+                var session = level.Session;
+                var data = session.MapData.Get(otherLoc.MapRoom);
+                Vector2 newRoomSpawnPoint = data.Spawns.ClosestTo(otherPos);
+                session.RespawnPoint = newRoomSpawnPoint;
+                var p = player.Position;
+                player.Position = newRoomSpawnPoint;
+
+                level.TransitionTo(data, (player.Position - p).SafeNormalize());
+            }
             player.Visible = false;
-            player.Position = playerWatching.State!.Position;
             player.StateMachine.State = Player.StFrozen;
-            player.ForceCameraUpdate = true;
-            player.DummyGravity = false;
-            level.EnforceBounds(player);
+
+            if (level.InCutscene && !level.SkippingCutscene)
+                level.SkipCutscene();
+
+            const int W = Celeste.GameWidth;
+            const int H = Celeste.GameHeight;
+
+            var cam = level.Camera;
+
+            Vector2 target = playerWatching.State.Position;
+            Vector2 camTarget = target - new Vector2(W, H) / 2f;
+            camTarget.X = MathHelper.Clamp(camTarget.X, level.Bounds.Left, level.Bounds.Right - W);
+            camTarget.Y = MathHelper.Clamp(camTarget.Y, level.Bounds.Top, level.Bounds.Bottom - H);
+            cam.Position = Calc.Approach(cam.Position, camTarget, ((cam.Position - camTarget).Length() * 4f) * Engine.RawDeltaTime);
         }
     }
 
@@ -48,5 +72,15 @@ public sealed partial class MainComponent
         }
 
         return player;
+    }
+
+    private static void GotoLevel(Level level, Player player, Vector2 at)
+    {
+        var session = level.Session;
+        var data = session.MapData.GetAt(at);
+        session.Level = data.Name;
+        session.RespawnPoint = data.Spawns.ClosestTo(at);
+        player.Position = session.RespawnPoint.Value;
+        level.LoadLevel(Player.IntroTypes.Transition);
     }
 }
