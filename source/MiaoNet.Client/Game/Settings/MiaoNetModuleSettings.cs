ace97cbb3e22eb9167e68bdc0943a1872cb0be72
Saplonily <Saplonily@outlook.com>
2026-08-20 15:47:24 +0800

    Remove encryption of saved token data

--- source/MiaoNet.Client/Game/Settings/MiaoNetModuleSettings.cs
+++ source/MiaoNet.Client/Game/Settings/MiaoNetModuleSettings.cs
@@ -18,27 +18,14 @@ public sealed class MiaoNetModuleSettings : EverestModuleSettings,
 
 #if USE_CELEMIAO_AUTH
 
-    // encrypted using the user's environment string so that 
-    // someone can't just leak it by taking a screenshot of the settings file.
     [YamlIgnore]
     public byte[]? TokenData { get; set; }
 
-    // This is for Serializer
-    // but we can't make it private...
-    public string? TokenDataEncrypted
+    // for serializer, byte[] will be serialized into an array of numbers in yaml
+    public string? TokenDataEncoded
     {
-        get => TokenData is null ? null : TokenDataUtils.Encrypt(TokenData);
-        set
-        {
-            if (value is null)
-            {
-                TokenData = null;
-                return;
-            }
-            TokenData = TokenDataUtils.TryDecrypt(value, out byte[]? tokenData)
-                ? tokenData
-                : null;
-        }
+        get => TokenData is null ? null : Convert.ToBase64String(TokenData);
+        set => TokenData = value is null ? null : Convert.FromBase64String(value);
     }
 
     public string? LastName { get; set; }
