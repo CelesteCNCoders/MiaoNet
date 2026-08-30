1f010426ac6ca00e89ada8f6361271d50280a8e2
wheatEars <1460592649@qq.com>
2026-08-30 15:00:29 +0800

    Remove [YamlIgnore] of NewMessageShowingMode so now it can be saved

--- source/MiaoNet.Client/Game/Settings/MiaoNetModuleSettings.cs
+++ source/MiaoNet.Client/Game/Settings/MiaoNetModuleSettings.cs
@@ -224,7 +224,6 @@ public sealed class MiaoNetModuleSettings : EverestModuleSettings,
 
     public bool PlayerPresenceMessages { get; set; } = true;
 
-    [YamlIgnore]
     public NewMessageShowingMode NewMessagesShowing
     {
         get;
