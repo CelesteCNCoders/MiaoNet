77cf3b1999054b41423ff762b367f0694a4068fe
Saplonily <Saplonily@outlook.com>
2026-09-16 20:45:26 +0800

    Fix PacketUpdateGlobalFlag write width mismatch (what)

--- source/MiaoNet.Shared/Packet/Packets/PacketUpdateGlobalFlag.cs
+++ source/MiaoNet.Shared/Packet/Packets/PacketUpdateGlobalFlag.cs
@@ -11,7 +11,7 @@ public sealed class PacketUpdateGlobalFlag : IContextlessPacket<PacketUpdateGlob
 
     public void Serialize(ref RefBinaryWriter writer)
     {
-        writer.Write((ushort)Flags);
+        writer.Write((byte)Flags);
     }
 
     public static PacketUpdateGlobalFlag Deserialize(ref RefBinaryReader reader)
